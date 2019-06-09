279057f4cdaba8ea53efc6c6427b4f8aabef8f9b
Ervin Chua <ervinchua@gmail.com>
2019-06-09 21:44:33 +1000

    I need to stop using the old hash syntax 🤮

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,8 +1,8 @@
 class TicketsController < ApplicationController
   def index
     @tickets, @errors, @prev, @next = Ticket.all(
-      :page => params[:page],
-      :per_page => params[:per_page]
+      page: params[:page],
+      per_page: params[:per_page]
     )
   end
 
